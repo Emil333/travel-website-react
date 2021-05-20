51962f64a7b7ea426ed136de1ceae64c845493cd
Emil <emilpradeep@gmail.com>
2021-05-20 13:33:39 +0530

    updated folder structure, moved images and videos to inside scr folder. Updated button styling and provided app.css styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,25 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Button } from './Button';
+import './Navbar.css'
 
 
 function Navbar() {
 
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false);
+        }else{
+            setButton(true);
+        }
+    }
+    window.addEventListener('resize', showButton)
 
     return (
       <>
@@ -19,7 +31,7 @@ function Navbar() {
             <div className="menu-icon" onClick={handleClick}>
               <i className={click ? "fas fa-times" : "fas fa-bars"} />
             </div>
-            <ul className={click ? "nav-menu active" : "nav-menu"}></ul>
+            <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
               <Link to="/" className="nav-links" onClick={closeMobileMenu}>
                 Home
@@ -40,6 +52,8 @@ function Navbar() {
               Sign-up
               </Link>
             </li>
+            </ul>
+            {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
           </div>
         </nav>
       </>
